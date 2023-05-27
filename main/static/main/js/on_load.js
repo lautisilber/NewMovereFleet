39c5b69816b31cb5841e56ddbcb3e924e05518c5
lautisilber <lautisilbergleit@gmail.com>
2023-05-27 03:35:32 -0300

    reworked production branch

--- main/static/main/js/on_load.js
+++
@@ -1,7 +0,0 @@
-function onLoad(cb) {
-    if(window.addEventListener) {
-        window.addEventListener('load', () => {cb()}); //W3C
-    } else {
-        window.attachEvent('onload', () => {cb()}); //IE
-    }
-}
