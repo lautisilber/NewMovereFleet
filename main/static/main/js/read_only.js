11127462202d3895be809c786c5d0d284e4ab03a
lautisilber <lautisilbergleit@gmail.com>
2023-05-27 04:13:22 -0300

    Deleted unused static files

--- main/static/main/js/read_only.js
+++
@@ -1,6 +0,0 @@
-Array.prototype.slice.call(document.querySelectorAll('input[type]:not([type="checkbox"]), textarea'),0).forEach(function(elem) {
-    elem.setAttribute('readonly', true);
-});
-Array.prototype.slice.call(document.querySelectorAll('input[type="checkbox"]'),0).forEach(function(elem) {
-    elem.setAttribute('onclick', 'return false;');
-});
